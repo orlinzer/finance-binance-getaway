363acea209de9c191d61c1221ec168723002503a
Or Linzer <orlinzer@gmail.com>
2025-03-07 17:27:12 +0200

    Add dependencies and implement data fetching functionality in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{/* web, */ App, HttpResponse, HttpServer, Responder, get, post};
+use actix_web::{
+    App, HttpResponse, HttpServer, Responder, cookie::time::error::Format, get, post, web,
+};
+use reqwest::{Response, Url};
+use std::time::Duration;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -44,21 +48,67 @@ async fn healthcheck() -> impl Responder {
 //     }
 // }
 
-// pub fn init(config: &mut web::ServiceConfig) {
-// config.service(
-//     web::scope("")
-//         .service(index)
-//         .service(healthcheck)
-// );
-// }
+// pub async fn fetch_data(symbol: &str) -> Result<CompanyQuote, Box<dyn std::error::Error>> {
+pub async fn fetch_data(symbol: &str) -> Result<String, Box<dyn std::error::Error>> {
+    println!("Fetching data...");
+
+    let url = format!(
+        "https://data-api.binance.vision/api/v3/exchangeInfo?symbol={}",
+        symbol
+    );
+    let url2 = format!(
+        "https://data-api.binance.vision/api/v3/trades?symbol={}",
+        symbol
+    );
+
+    let url = Url::parse(&*url)?;
+    let url2 = Url::parse(&*url2)?;
+    // let res = reqwest::get(url).await?.json::<CompanyQuote>().await?;
+    let res = reqwest::get(url).await?.text().await?;
+    let res2 = reqwest::get(url2).await?.text().await?;
+
+    // println!("Data fetched! {:?}", res2);
+
+    let result = format!("{:?}, {:?}", res, res2);
+
+    Ok(result)
+}
+
+pub fn init(config: &mut web::ServiceConfig) {
+    config
+        // .service(web::scope("")
+        // .service(index)
+        // .service(healthcheck));
+        .service(hello)
+        .service(echo);
+    // .route("/hey", web::get().to(manual_hello))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    tokio::spawn(async move {
+        let mut interval = tokio::time::interval(Duration::from_millis(1000));
+        loop {
+            interval.tick().await; // This should go first.
+
+            let res = fetch_data("BTCUSDT").await;
+            match res {
+                Ok(data) => {
+                    // println!("Data: {:?}", data);
+                    println!("Data: {:?}", data);
+                }
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                }
+            }
+            // tokio::spawn(fetch_data());
+        }
+    });
+
     HttpServer::new(|| {
-        App::new()
-            // .configure(init)
-            .service(hello)
-            .service(echo)
+        App::new().configure(init)
+        // .service(hello)
+        // .service(echo)
         // .route("/hey", web::get().to(manual_hello))
     })
     .bind("0.0.0.0:4000")?
